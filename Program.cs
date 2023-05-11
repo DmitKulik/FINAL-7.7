fa4dabc13b357e9e1ae442fabb4d5ab06d72be1c
Dmitry <grammattika@gmail.com>
2023-05-11 23:26:27 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,35 +1,40 @@
 ﻿namespace FINAL_7._7{
     internal class Program{
         static void Main(){
-            Console.WriteLine("77");
+            /* ... */
         }
-        abstract class Delivery{
+
+        class AllPersons{
+            public string Name;
+            public string Surname;
+        }
+        abstract class Delivery{ // абстрактный класс
             public string Address;
 
-            protected virtual void DisplayStatus(){
-                Console.WriteLine("Доставка");
+            protected virtual void ShippingType(){ // protected модификатор метода
+                /* ... */ // реализация метода
             }
         }
 
-        class HomeDelivery : Delivery{
-            protected override void DisplayStatus(){
-                Console.WriteLine("Доставка на дом");
+        class HomeDelivery : Delivery{ // наследование
+            protected override void ShippingType(){ // protected модификатор метода, c новой реализацией
+                /* ... */ // реализация метода
             }
         }
 
-        class PickPointDelivery : Delivery{
-            protected override void DisplayStatus(){
-                Console.WriteLine("Доставка в пункт выдачи");
+        class PickPointDelivery : Delivery{ // наследование
+            protected override void ShippingType(){ // protected модификатор метода, c новой реализацией
+                /* ... */ // реализация метода
             }
         }
 
-        class ShopDelivery : Delivery{
-            protected override void DisplayStatus(){
-                Console.WriteLine("Доставка в магазин");
+        class ShopDelivery : Delivery{ // наследование
+            protected override void ShippingType(){ // protected модификатор метода, c новой реализацией
+                /* ... */ // реализация метода
             }
         }
 
-        class Order<TDelivery, TStruct> where TDelivery : Delivery{
+        class Order<TDelivery, TStruct> where TDelivery : Delivery{ // обобщение
             public TDelivery Delivery;
 
             public int Number;
@@ -41,6 +46,20 @@
             }
         }
 
-        
+        class Customer : AllPersons{
+            private int Age;
+            private string Phone;
+            /* ..
+             * 
+             * 
+             * 
+             * . */
+
+
+            public int VerificationAge{get{return Age;} set{if (value <= 20){ Console.WriteLine("Не подходящий возраст для покупки алкоголя");} else{Age = value;}
+                }
+            }
+
+        }
     }
 }
